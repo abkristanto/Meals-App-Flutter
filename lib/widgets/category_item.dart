58c383f09fffba1baab5e586026ef2bd67c31ceb
Abraham Kristanto <abkristanto@gmail.com>
2021-05-15 23:09:24 +0700

    Add Meal Item Widget

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String id;
   final String title;
@@ -8,7 +10,7 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext context) {
-    Navigator.of(context).pushNamed('/category-meals', arguments: {
+    Navigator.of(context).pushNamed(CategoryMealsScreen.routeName, arguments: {
       'id': id,
       'title': title,
     });
